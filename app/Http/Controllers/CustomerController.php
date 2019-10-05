c5812815af1aa13b3d4823cd413c34c2c31362c2
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2019-10-05 09:46:32 +0200

    Refactoring the code
    Added a form.blade.php file where i can edit all the forms on the site. Prevent repeating code.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,61 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     public function index()
     {
-        $customers = \App\Customer::all();
+        $customers = Customer::all();
 
         return view('customer.index', compact('customers'));
     }
 
     public function create()
     {
-        return view('customer.create');
+        $customer = new Customer();
+
+        return view('customer.create', compact('customer'));
     }
 
     public function store()
     {
-        $data = request()->validate([
-            'name' => 'required',
-            'email' => 'required|email'
-        ]);
 
-        \App\Customer::create($data);
+        Customer::create($this->vailidatedData());
 
         return redirect('/customers');
 
     }
 
-    public function show(\App\Customer $customer)
+    public function show(Customer $customer)
     {
        return view('customer.show', compact('customer'));
     }
 
-    public function edit(\App\Customer $customer)
+    public function edit(Customer $customer)
     {
         return view('customer.edit', compact('customer'));
     }
 
-    public function update(\App\Customer $customer)
+    public function update(Customer $customer)
     {
-        $data = request()->validate([
-            'name' => 'required',
-            'email' => 'required|email'
-        ]);
 
-        $customer->update($data);
+        $customer->update($this->vailidatedData());
 
         return redirect('/customers');
     }
 
-    public function destroy(\App\Customer $customer)
+    public function destroy(Customer $customer)
     {
         $customer->delete();
 
         return redirect('/customers');
+    }
+
+    protected function vailidatedData()
+    {
+        return request()->validate([
+            'name' => 'required',
+            'email' => 'required|email'
+        ]);
     }
 }
--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -2,22 +2,8 @@
 
 <form method="post" action="/customers" >
 
-    <div>
-    <label for="name">Name</label>
-    <input type="text" name="name" autocomplete="off" value="{{ old('name') }}">
-    @error('name') <p style="color: red">{{ $message }}</p> @enderror 
-    </div>
+    @include('customer.form')
 
-
-    <div>
-    <label for="email">Email</label>
-    <input type="text" name="email" autocomplete="off"  value="{{ old('email') }}">
-    @error('email') <p style="color: red">{{ $message }}</p> @enderror 
-    </div>
-
-    @csrf
-
-    <button>Add New Customer</button
-    >
+    <button>Add New Customer</button>
 
 </form> 
