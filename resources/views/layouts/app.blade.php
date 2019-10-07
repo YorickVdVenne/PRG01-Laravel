ad47d05ed7171fbd97709daac349d3cad5266667
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2019-10-07 17:31:23 +0200

    Name changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
                     <div><img src="/png/foodStarLogo.png" style="height: 23px; border-right: 1px solid #333" class="pr-3"></div>
-                    <div class="pl-3">Foodstar</div>
+                    <div class="pl-3">FoodStar</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
