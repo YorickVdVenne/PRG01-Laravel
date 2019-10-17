b6262343045d7f0b646545d7bab3ac30b95641cc
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2019-10-17 11:34:22 +0100

    New layout for welcome page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/restaurants';
 
     /**
      * Create a new controller instance.
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,4 +1,5 @@
 <ul>
+        <li><a href="/">Home</a></li>
         <li><a href="/about">About Us</a></li>
-        <li><a href="/restaurant">Restaurants</a></li>
+        <li><a href="/restaurants">Restaurants</a></li>
     </ul>
