9804ac5f22e06d727d8c03fe23f513f256715d73
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2019-10-19 12:58:55 +0200

    Redirect corrections

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,10 +17,19 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/restaurants');
+        switch ($guard) {
+            case 'admin':
+                if (Auth::guard($guard)->check()) {
+                    return redirect()->route('admin.dashboard');
+                }           
+                break;
+            
+            default:
+                if (Auth::guard($guard)->check()) {
+                    return redirect('/restaurants');
+            }    
+                break;
         }
-
         return $next($request);
     }
 }
