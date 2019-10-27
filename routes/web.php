1013af27c2f8abc93fc6fc45603f5cbbd286a01c
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2019-10-27 21:09:19 +0100

    Deleting unused controller - cleaning up

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', 'HelloController@about'); 
-
 Route::get('/restaurants', 'RestaurantController@index');
 
 Auth::routes();
 
-Route::get('/profile/{user}', 'ProfileController@index')->name('profile.show');
-Route::get('/profile/{user}/edit', 'ProfileController@edit');
-Route::patch('/profile/{user}', 'ProfileController@update');
+Route::prefix('/profile')->group(function() {
+    Route::get('/{user}', 'ProfileController@index')->name('profile.show');
+    Route::get('/{user}/edit', 'ProfileController@edit');
+    Route::patch('/{user}', 'ProfileController@update');
+});
 
+Route::prefix('/admin')->group(function() {
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
 
-    Route::prefix('admin')->group(function() {
-        Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-        Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-        Route::get('/', 'AdminController@index')->name('admin.dashboard');
-
-        Route::get('/restaurants/create', 'AdminController@create');
-        Route::post('/', 'AdminController@store');
-        Route::get('/{restaurant}', 'AdminController@show');
-        Route::get('/{restaurant}/edit', 'AdminController@edit');
-        Route::patch('/{restaurant}', 'AdminController@update');
-        Route::delete('/{restaurant}', 'AdminController@destroy');
-    });
+    Route::get('/restaurants/create', 'AdminController@create');
+    Route::post('/', 'AdminController@store');
+    Route::get('/{restaurant}', 'AdminController@show');
+    Route::get('/{restaurant}/edit', 'AdminController@edit');
+    Route::patch('/{restaurant}', 'AdminController@update');
+    Route::delete('/{restaurant}', 'AdminController@destroy');
+});
