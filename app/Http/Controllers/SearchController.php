e7f05c7b1f74bbc5722e4b57043328a60bad6021
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2019-10-30 19:48:44 +0100

    Variable name change

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,11 +9,11 @@ class SearchController extends Controller
 {
     public function search()
     {
-        $q = Request::get ( 'q' );
-        $restaurant = Restaurant::where('name','LIKE','%'.$q.'%')->orWhere('category','LIKE','%'.$q.'%')->get();
+        $search = Request::get ( 'search' );
+        $restaurant = Restaurant::where('name','LIKE','%'.$search.'%')->orWhere('category','LIKE','%'.$search.'%')->get();
 
         if(count($restaurant) > 0)
-        return view('search')->withDetails($restaurant)->withQuery ( $q );
+        return view('search')->withDetails($restaurant)->withQuery ( $search );
 
         else return view ('search', [ 
             'errorMessage' => 'No Restaurants found. Try to search again!',
--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -5,7 +5,7 @@
 <form action="/search" method="POST" role="search">
     @csrf
     <div class="input-group">
-        <input type="text" class="form-control" name="q" autocomplete="off"
+        <input type="text" class="form-control" name="search" autocomplete="off"
             placeholder="Search restaurants"> <span class="input-group-btn">
             <button type="submit" class="btn btn-default">
                 <span class="glyphicon glyphicon-search"></span>
